3e1f10c1323ef1ca0ea3ee7b4da7f3210c480225
Vibha Rana <ranavibha02@gmail.com>
2020-02-16 13:55:37 -0800

    entered the privacy link

---
+++ roleDemo/Models/RoleVm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace roleDemo.Models
+{
+    public class RoleVm
+    {
+
+        public string Id { get; set; }
+       // [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+
+    }
+}
